5d789f479ed7724e3af55bf478e2f316ec7806b8
Home <Home@DESKTOP-C0B5N2B>
2023-06-28 13:49:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Convert.ToInt32(Console.ReadLine()) > 7) Console.WriteLine("Привет");
+            Console.ReadLine();
+        }
+    }
+}
